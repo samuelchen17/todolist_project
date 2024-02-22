496e2f9c7edc7d38ed3f1d0c122da7c5a695d3c7
Samuel Chen <sche842@gmail.com>
2024-02-22 18:22:45 +1100

    To do list finished

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const todoContainer =  document.querySelector('.todoContainer')
 
 let todoList = []
 
+// load inital page UI based on local storage
+function initialLoad() {
+    if (!localStorage.getItem('todos')) {return} // return if todos doesnt exist
+    todoList = JSON.parse(localStorage.getItem('todos')).todoList
+    updateUI()
+}
+
+initialLoad()
+
 function addTodo() {
     const todo = textarea.value
     if (!todo) { return }
@@ -13,23 +22,47 @@ function addTodo() {
     updateUI()
 }
 
+function editTodo(index) {
+    textarea.value = todoList[index]
+    todoList = todoList.filter((element, elementIndex) => {
+        if (index === elementIndex) {return false}
+        return true
+    })
+    updateUI()
+}
+
+function deleteTodo(index) {
+    todoList = todoList.filter((element, elementIndex) => {
+        if (index === elementIndex) {return false}
+        return true
+    })
+    updateUI()
+}
+
 // loop over every item in todoList and add some HTML to the container
 function updateUI() {
     let newInnerHTML = ''
 
-    todoList.forEach((todoElement) => {
+    todoList.forEach((todoElement, todoIndex) => {
         newInnerHTML += `
             <div class="todo">
                 <p>${todoElement}</p>
                 <div class="btnContainer">
-                    <button class="iconBtn"><i class="fa-solid fa-pen-to-square"></i></button>
-                    <button class="iconBtn"><i class="fa-solid fa-trash"></i></button>
+                    <button class="iconBtn" onclick="editTodo(${todoIndex})">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+                    <button class="iconBtn" onclick="deleteTodo(${todoIndex})">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
                 </div>
             </div>
         `
     })
     
     todoContainer.innerHTML = newInnerHTML
+
+    // save to local storage
+    localStorage.setItem('todos', JSON.stringify({ todoList }))
 }
 
 addBtn.addEventListener('click', addTodo)
